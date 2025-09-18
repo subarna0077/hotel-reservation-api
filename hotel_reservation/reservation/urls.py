88f68c44ef9176ecf964ac9004a2bdddd5013859
subh077 <88318416+subarna0077@users.noreply.github.com>
2025-09-19 01:59:00 +0545

    Updated urls
    -- Added jwt token urls in main urls.py
    -- Added the nested url (booking/<booking:id>/payments)
    -- Added esewa/callback route for esewa testing.

--- hotel_reservation/reservation/urls.py
+++ hotel_reservation/reservation/urls.py
@@ -1,17 +1,22 @@
 from django.urls import path, include
-from .views import UserCreateView, HotelView, RoomView, ReviewView
+from .views import UserCreateView, HotelView, RoomView, ReviewView, BookingView, PaymentView, callback_esewa
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedDefaultRouter
 router = DefaultRouter()
 router.register('hotels', HotelView, basename='hotel')
+router.register('bookings', BookingView, basename='bookings')
 hotels_router = NestedDefaultRouter(router, 'hotels', lookup='hotel')
 hotels_router.register('rooms',RoomView, basename='hotel-rooms')
 hotels_router.register('reviews', ReviewView, basename='hotel-reviews')
+booking_router = NestedDefaultRouter(router,'bookings', lookup='booking')
+booking_router.register('payments', PaymentView, basename='booking-payments')
 
 urlpatterns = [
     path('create-user/', UserCreateView.as_view(), name='create-user'),
     path('', include(router.urls)),
-    path('', include(hotels_router.urls))
+    path('', include(hotels_router.urls)),
+    path('', include(booking_router.urls)),
+    path('esewa/callback/', callback_esewa, name='esewa-callback')
 ]
 
 
