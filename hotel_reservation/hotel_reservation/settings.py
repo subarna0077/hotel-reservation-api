b2721c78aac8096339f7a09ecee404187d333b08
subh077 <88318416+subarna0077@users.noreply.github.com>
2025-09-19 01:56:35 +0545

    Settings update
    -- Added custom user model to the settings.
    -- Added JWT settings to overide the token lifetime

--- hotel_reservation/hotel_reservation/settings.py
+++ hotel_reservation/hotel_reservation/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'reservation',
     'rest_framework',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -128,4 +130,16 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
+}
+
+AUTH_USER_MODEL = 'reservation.User'
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=45),  # default 15 minutes
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),     # change this to increase refresh token lifetime
+    "ROTATE_REFRESH_TOKENS": True,                  # optional
+    "BLACKLIST_AFTER_ROTATION": True,              # optional
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "AUTH_HEADER_TYPES": ("Bearer",),
 }
